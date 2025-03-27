2f0f3306d034b213df86b625ebb055c8eb1dcdcf
silvanicum <silvanicum@gmail.com>
2025-03-27 21:31:11 +0200

    Update server.js

--- server.js
+++ server.js
@@ -7,6 +7,11 @@ app.get("/", (req, res) => {
     res.send("Hello from Render!");
 });
 
+// Új útvonal a /chat-hez
+app.get("/chat", (req, res) => {
+    res.send("Ez a chat oldal!");
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
